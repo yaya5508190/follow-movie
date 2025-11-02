306f93440ef5c9c28a86bad5e890dedc8b8c4732
yx <a357375084@gmail.com>
2025-11-02 17:44:51 +0800

    pre-auth配置支持删除

--- plugins/pre-auth/z-space/fronted-web/src/App.vue
+++ plugins/pre-auth/z-space/fronted-web/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <pre-auth-setting :action="'update'" :id="1"/>
+      <pre-auth-setting :action="'update'" :id="10"/>
     </v-main>
   </v-app>
 </template>
